167aa1d7d270cbb5d9e0789104127e9958e7d004
gauravv8090 <gaurav8090983461@gmail.com>
2023-01-09 14:22:32 +0530

    Work in progress

---
+++ src/components/Inputs.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { UilSearch, UilLocationPoint } from "@iconscout/react-unicons";
+
+function Inputs() {
+  return (
+    <div className="flex flex-row justify-center my-6 ">
+      <div className="flex flex w-3/4  items-center justify-center space-x-4 ">
+        <input
+          type="text"
+          placeholder="Search..."
+          className=" text-xl font-light p-2 focus:outline-none capitalize w-full shadow-xl placeholder:lowercase"
+        />
+        <UilSearch size={25} className='text-white cursor-pointer transition ease-out hover:scale-125' />
+        <UilLocationPoint size={25} className='text-white cursor-pointer transition ease-out hover:scale-125' />
+      </div>
+
+      <div className='flex flex-row w-1/4 items-center justify-center '  >
+        <button name="metric" className='text-xl text-white font-light' >&deg;C</button>
+        <p className='text-xl text-white mx-1' >|</p>
+        <button name="imperial" className='text-xl text-white font-light '>&deg;F </button>
+
+      </div>
+    </div>
+  );
+}
+
+export default Inputs;
---
+++ src/components/TopButtons.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+function TopButtons() {
+
+    const cities = [
+        {
+            id: 1,
+            title: 'London'
+        },
+        {
+            id: 2,
+            title: 'China'
+        },
+        {
+            id: 3,
+            title: 'Japan'
+        },
+        {
+            id: 4,
+            title: 'Korea'
+        },
+        {
+            id: 5,
+            title: 'Tokyo'
+        },
+    ]
+
+
+  return (
+    <div className='flex items-center justify-around my-6' >
+        {
+            cities.map((city)=>(
+                <button key={city.id} className='text-white text-lg font-medium' >{city.title}</button>
+            ))
+        }
+    </div>
+  )
+}
+
+export default TopButtons
