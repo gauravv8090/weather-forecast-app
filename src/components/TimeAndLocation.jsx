2fd83cca5896ad18fc51a1195a0662781d1ef01a
gauravv8090 <gaurav8090983461@gmail.com>
2023-01-09 21:32:47 +0530

    work in progress

--- src/components/TimeAndLocation.jsx
+++ src/components/TimeAndLocation.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import React from "react";
 
 function TimeAndLocation() {
-  return <div className='' >
-    <div className='flex items-center justify-center my-6' >
-
-        <p className='text-white text-xl font-extralight  ' >
-         Tuesday, 31 May 2022 | Location time: 12:46 PM    
+  return (
+    <div className="">
+      <div className="flex items-center justify-center my-6">
+        <p className="text-white text-xl font-extralight">
+          Tuesday, 31 May 2022 | Location time: 12:46 PM
         </p>
-
+      </div>
+      <div className='flex items-center justify-center my-3'>
+        <p className='text-white text-3xl font-medium ' >Berlin, DE</p>
+      </div>
     </div>
-  </div>
+  );
 }
 
-export default TimeAndLocation
+export default TimeAndLocation;
